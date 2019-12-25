87bbab664af54eb7c37f7a4ffa7f4403ffdcc165
jebbe <zhaojie@oppo.com>
2019-12-25 17:18:47 +0800

    initial pelican commit to content

---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Jebbe'
+SITENAME = u'Jebbe Blog'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = u'zh'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('HDiffPatch', 'https://github.com/CheihChiu/HDiffPatch'),
+         ('亚马逊验证码破解', 'https://github.com/CheihChiu/amazon_captcha_break'),
+         ('Qigsaw', 'https://github.com/CheihChiu/Qigsaw'),
+         ('等待更多', '#'),)
+
+# Social widget
+SOCIAL = (('邮箱', 'zhaojierb#qq.com'),
+          ('其他', '#'),)
+
+DEFAULT_PAGINATION = 20
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
